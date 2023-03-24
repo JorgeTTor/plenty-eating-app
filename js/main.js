9c4eab91ccaee4ca8f508d472f3dc28b9c903349
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2023-03-24 17:32:20 -0600

    [ADD] base url const

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ const container = document.querySelector('.recipes__container');
 let searchQuery = ' ';
 const PLENTYAPP_ID = "0b5b5122";
 const PLENTYAPP_key = "6fc7e1d068c388df98233ed5c0894b19";
+const baseURL = ` https://api.edamam.com/api/recipes/v2?q=pizza&app_id=${PLENTYAPP_ID}&app_key=${PLENTYAPP_key}`;
+
 
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector('input').value;
   console.log(searchQuery)
+  console.log(searchQuery)
+  fetchAPI();
 })
+
+async function fetchAPI()
