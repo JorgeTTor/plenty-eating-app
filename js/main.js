7a2d5547af56b692bce88127dad552e4a2394f33
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2023-03-24 17:56:55 -0600

    [FIX] baseURL code

--- js/main.js
+++ js/main.js
@@ -4,16 +4,21 @@ const container = document.querySelector('.recipes__container');
 let searchQuery = ' ';
 const PLENTYAPP_ID = "0b5b5122";
 const PLENTYAPP_key = "6fc7e1d068c388df98233ed5c0894b19";
-const baseURL = ` https://api.edamam.com/api/recipes/v2?q=pizza&app_id=${PLENTYAPP_ID}&app_key=${PLENTYAPP_key}`;
-
 
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector('input').value;
   console.log(searchQuery)
-  console.log(searchQuery)
   fetchAPI();
 })
 
-async function fetchAPI()
+async function fetchAPI() {
+  const baseURL = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${PLENTYAPP_ID}&app_key=${PLENTYAPP_key}&to=20`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  createHTML()
+  console.log(data)
+
+}
+
