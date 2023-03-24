8c8321d013b40023e89e2afd73a7e031f810e40e
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2023-03-24 17:21:11 -0600

    [ADD] api id & key vars

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const searchForm = document.querySelector('form');
 const searchResult = document.querySelector('.recipes__result');
 const container = document.querySelector('.recipes__container');
 let searchQuery = ' ';
+const PLENTYAPP_ID = "0b5b5122";
+const PLENTYAPP_key = "6fc7e1d068c388df98233ed5c0894b19";
+
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
