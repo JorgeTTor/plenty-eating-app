f2e9bc003cffe4c13f2768f959042ce1256c9615
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2023-03-24 18:23:43 -0600

    [ADD] render html

--- js/main.js
+++ js/main.js
@@ -17,8 +17,24 @@ async function fetchAPI() {
   const baseURL = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${PLENTYAPP_ID}&app_key=${PLENTYAPP_key}&to=20`;
   const response = await fetch(baseURL);
   const data = await response.json();
-  createHTML()
+  createHTML(data.hits)
   console.log(data)
 
 }
-
+function createHTML(results) {
+  let renderHTML = '';
+  results.map(result => {
+    renderHTML += 
+    `
+    <div class="recipe__item">
+    <img src="/images/healthy_salad.jpg" alt="healthy salad" class="recipe__img">
+    <div class="recipe__content">
+      <h2 class="subtitle">This is the recipe</h2>
+      <a href="#" class="recipe__btn">View Recipe</a>
+    </div>
+    <p class="recipe__data">Calories: 115</p>
+  </div>
+    `
+  })
+  searchResult.innerHTML = renderHTML;
+}
